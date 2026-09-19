fdc70fe461e1c71146de5f0f8821e977028f5d16
salempub1-dotcom <salempub1@gmail.com>
2026-09-19 04:39:54 +0100

    Load Today V2.2 finishing styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './teacher-pages.css';
 import './v2.css';
 import './lesson-v2.css';
 import './v2-polish.css';
+import './v2-2.css';
 
 export const metadata: Metadata = {
   title: 'معراج الأستاذ',
